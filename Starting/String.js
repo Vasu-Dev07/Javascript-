d57fda26dfd273a99aae2b19cedee93bc861dff1
VasuDev987 <bishwjitgorai1@gmail.com>
2026-08-11 23:33:05 +0530

    String and Operation

---
+++ Starting/String.js
@@ -0,0 +1,44 @@
+// String
+// const str1 = "Rohitboy";
+// const str2 = 'Rohit Negi';
+// const day = 18;
+// const str3 = `Strike is coming on ${day}`;
+
+// console.log(str3);
+
+// const str = `Hello Coder Army`;
+
+// console.log(str.length)
+// console.log(str[1]);
+
+// str[2] = "S";
+// console.log(str)
+
+// upper case
+// const a = str.toUpperCase();
+// const b = str.toLowerCase();
+// console.log(b);
+
+// const str = `Hello Coder Army Coder`;
+
+// console.log(str.indexOf('Cod'));
+// console.log(str.lastIndexOf('Cod'));
+// console.log(str.includes('cod'));
+
+// slice 
+// console.log(str.slice(2,7));
+// console.log(str.slice(3));
+// console.log(str.slice(-5,-2));
+
+// const str = `Hello Coder Army Coder`;
+
+// console.log(str.substring(2,5));
+
+// const a = "Rohit";
+// const b = "Negi";
+// const c = a+" "+b;
+// console.log(c);
+
+// console.log(24+"Rohit");
+// console.log(24+"Rohit"+10);
+// console.log(24+30+"Rohit");
