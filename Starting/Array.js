e741745361f03f6fd674dee2139b163399dab48f
VasuDev987 <bishwjitgorai1@gmail.com>
2026-08-14 23:37:17 +0530

    Introduction to Arrayy..

---
+++ Starting/Array.js
@@ -0,0 +1,32 @@
+// Array;
+
+// let marks1 = 100;
+// let marks2 = 50;
+// let marks3 = 70;
+// let marks4 = 80;
+
+let marks = [100,50,70,80,90];
+
+
+
+console.log(marks);
+console.log(marks.length);
+
+let arr = [100,30,"Rohit",true];
+
+// console.log(arr[2]);
+// console.log(typeof arr);
+
+// arr[1] = 90;
+// console.log(arr);
+
+// push : Insert element at end
+
+arr.push(90);
+arr.push("Strike");
+
+console.log(arr);
+
+// pop operation: Delete element from end
+arr.pop();
+console.log(arr);
