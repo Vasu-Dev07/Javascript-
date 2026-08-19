3e3e417d1572fc98bb37b394e4a65dae03ef92a6
VasuDev987 <bishwjitgorai1@gmail.com>
2026-08-19 23:18:47 +0530

    Functions in Objects

--- Starting/Objects.js
+++ Starting/Objects.js
@@ -29,12 +29,12 @@
 // delete user.emailId;
 // console.log(user);
 
-const user = {
-   name: "Vasu",
-   age: 20,
-   emailId: "vasu@gmail.com",
-   amount: 3400,
-}
+// const user = {
+//    name: "Vasu",
+//    age: 20,
+//    emailId: "vasu@gmail.com",
+//    amount: 3400,
+// }
 
 // const user2 = user;
 // user2.age = 90;
@@ -54,39 +54,52 @@ const user = {
  //// user.keys user["name"] user["age"]
 
 // for of loop array ke
-const temnparr = Object.keys(user);
-console.log(temnparr)
-for(let keys of temnparr){
-    console.log(keys);
+// const temnparr = Object.keys(user);
+// console.log(temnparr)
+// for(let keys of temnparr){
+//     console.log(keys);
+// }
+
+// for(let values of Object.values(user)){
+//     console.log(values);
+// }
+
+// console.log(Object.entries(user));
+// [
+//   [ 'name', 'Vasu' ],
+//   [ 'age', 20 ],
+//   [ 'emailId', 'vasu@gmail.com' ],
+//   [ 'amount', 3400 ]
+// ]
+
+// for(let [keys,values] of Object.entries(user)){
+//     console.log(keys, values);
+
+// }
+
+
+// const name = user.name;
+// const age = user.age;
+
+// // object ko destructing karna
+// const {name:userName,age:userAge} = user;
+
+// const arr = [10,20,40,90,11];
+
+// const [first,second]= arr;
+
+// console.log(userName,userAge);
+// console.log(first,second);
+
+const user = {
+   name: "Vasu",
+   age: 20,
+   emailId: "vasu@gmail.com",
+   amount: 3400,
+   greeting: function(){
+    console.log(`Hello There WassUppp  ${this.name}`);
+    return 20;
+   }
 }
 
-for(let values of Object.values(user)){
-    console.log(values);
-}
-
-console.log(Object.entries(user));
-[
-  [ 'name', 'Rohit' ],
-  [ 'age', 20 ],
-  [ 'emailId', 'negi@gmail.com' ],
-  [ 'amount', 3400 ]
-]
-
-for(let [keys,values] of Object.entries(user)){
-    console.log(keys, values);
-
-}
-
-
-const name = user.name;
-const age = user.age;
-
-// object ko destructing karna
-const {name:userName,age:userAge} = user;
-
-const arr = [10,20,40,90,11];
-
-const [first,second]= arr;
-
-console.log(userName,userAge);
-console.log(first,second);
+user.greeting();
