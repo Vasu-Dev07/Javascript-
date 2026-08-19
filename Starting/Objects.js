9e459b1af5b7549faa7668f7e6bf285092be600b
VasuDev987 <bishwjitgorai1@gmail.com>
2026-08-19 23:11:35 +0530

    Global Execution Context

--- Starting/Objects.js
+++ Starting/Objects.js
@@ -1,54 +1,92 @@
-// object
-// key value
+// // object
+// // key value
+// const user = {
+//    name: "Vasu",
+//    age: 20,
+//    emailId: "vasu@gmail.com",
+//    amount: 3400,
+//    "home address": "dhanbad"
+// }
+
+
+// console.log(user["name"]);
+// console.log(user.age);
+// console.log(user["age"]);
+// console.log(user["home address"]);
+// console.log(typeof user);
+// // CRUD operation: Create read update delete
+
+
+// console.log(user.age);
+
+// //  update
+// user.aadhar = 1234;
+// user.amount = 5000;
+
+// console.log(user);
+
+// // delete
+// delete user.emailId;
+// console.log(user);
+
 const user = {
    name: "Vasu",
    age: 20,
    emailId: "vasu@gmail.com",
    amount: 3400,
-   "home address": "dhanbad"
 }
 
+// const user2 = user;
+// user2.age = 90;
 
-console.log(user["name"]);
-console.log(user.age);
-console.log(user["age"]);
-console.log(user["home address"]);
-console.log(typeof user);
-// CRUD operation: Create read update delete
+// console.log(user);
 
+// // Important
 
-console.log(user.age);
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
 
-//  update
-user.aadhar = 1234;
-user.amount = 5000;
+// for(let keys in user){
+//     console.log(keys, user[keys]);
+// }
 
-console.log(user);
+ //// user.keys user["name"] user["age"]
 
-// delete
-delete user.emailId;
-console.log(user);
-
-const user = {
-   name: "Rohit",
-   age: 20,
-   emailId: "negi@gmail.com",
-   amount: 3400,
+// for of loop array ke
+const temnparr = Object.keys(user);
+console.log(temnparr)
+for(let keys of temnparr){
+    console.log(keys);
 }
 
-const user2 = user;
-user2.age = 90;
+for(let values of Object.values(user)){
+    console.log(values);
+}
 
-console.log(user);
-
-Important
-
-console.log(Object.keys(user));
-console.log(Object.values(user));
 console.log(Object.entries(user));
+[
+  [ 'name', 'Rohit' ],
+  [ 'age', 20 ],
+  [ 'emailId', 'negi@gmail.com' ],
+  [ 'amount', 3400 ]
+]
 
-for(let keys in user){
-    console.log(keys, user[keys]);
+for(let [keys,values] of Object.entries(user)){
+    console.log(keys, values);
+
 }
 
-user.keys user["name"] user["age"]
+
+const name = user.name;
+const age = user.age;
+
+// object ko destructing karna
+const {name:userName,age:userAge} = user;
+
+const arr = [10,20,40,90,11];
+
+const [first,second]= arr;
+
+console.log(userName,userAge);
+console.log(first,second);
