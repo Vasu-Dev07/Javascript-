59dee7ab85bee15e80311c1f7a1cb0384313eb41
VasuDev987 <bishwjitgorai1@gmail.com>
2026-08-25 21:04:27 +0530

    introduction to function

---
+++ Starting/Function.js
@@ -0,0 +1,16 @@
+//  function
+
+// function greeting(){
+//     console.log("Hello Coder Army, Strike is coming on 18 october");
+//     return 10;
+// }
+
+
+// function addNumber(num1,num2,num3=0,num4=0){
+    
+//     const sum = num1+num2+num3+num4;
+//     console.log(sum);
+// }
+
+// greeting();
+// addNumber(3,4);
