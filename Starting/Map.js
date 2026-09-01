9ac2e018fc6c0331d76d0573e63541f5a80bf0d1
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-01 19:22:22 +0530

    filter kring

--- Starting/Map.js
+++ Starting/Map.js
@@ -18,3 +18,42 @@
 // }
 
 // obj.deposit(100);
+
+ //// filter
+
+// const arr = [10,20,30,5,90,87];
+// true: select, false: not selected
+// const newArr = arr.filter((number)=> number>25);
+
+// console.log(newArr);
+// this = [10,20,30,5,90,87];
+
+// const compare = (number)=> number>25;
+// compare(20);
+
+// Array.prototype.filtering = function(compare){
+    
+//     const ans = [];
+//     for(let num of this){
+//        if(compare(num)){
+//         ans.push(num);
+//        }
+//     }
+
+//     return ans;
+// }
+
+// const newArr = arr.filtering((num)=>num>25);
+
+// console.log(newArr);
+
+// const a = [80,30,15,31,42,70]
+// const answer = a.filtering((num)=>num>25);
+// console.log(answer);
+
+
+// const arr = [10,20,30,5,90,87];
+
+
+// const newArr = arr.map((num)=> num*3);
+// console.log(newArr);
