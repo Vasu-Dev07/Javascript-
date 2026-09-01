91b1f257745970e1c15c3686ec564fb4d09e456d
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-01 13:09:06 +0530

    scope of variables

---
+++ Starting/ScopeClosure.js
@@ -0,0 +1,45 @@
+// Scope and closure , HOF
+// Global_> Accessible to everyone
+// functional -> accesible only to that function
+// Block level scope-> accesible only to that Block
+
+// let a = 10;
+// const b = 20;
+
+
+
+// if(true){
+//     let d = 30;
+    
+// }
+
+ 
+
+// function greet(){
+
+//     let c = 30;
+//    var e = 90;
+// }
+
+// // console.log(c);
+// console.log(e);
+// greet();
+
+
+// let global = 30;
+
+
+// function greet(){
+     
+//     let global = 40;
+   
+
+//     function meet(){
+//         let global = 10;
+//         console.log(global);
+//     }
+    
+//     meet();
+// }
+
+// greet();
