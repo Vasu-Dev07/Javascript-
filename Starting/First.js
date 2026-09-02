48041083a1e1bee77a16e1faff5136f0cf6f8390
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-02 15:23:18 +0530

    jh

--- Starting/First.js
+++ Starting/First.js
@@ -70,9 +70,9 @@ const month = document.getElementById("ten");
 
 // console.log(month.children);
 const lister = document.createElement("li");
-lister.textContent = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>";
+lister.textContent = "<img src='https://plus.kjojhxhxhchww&fm=jpg&q=60&w=3000'>";
 
-// lister.innerHTML = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>"
+// lister.innerHTML = "<img src='https://plus.kjojhxhxhchww&fm=jpg&q=60&w=3000'>"
 
 month.prepend(lister);
 // month.insertAdjacentElement("afterend",lister)
