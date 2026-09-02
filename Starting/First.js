45d5c1bd84407c8cb4032238f61e3c2070ce4845
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-02 15:19:25 +0530

    we basically use textContent instead of inner.Html coz of lochaa..

--- Starting/First.js
+++ Starting/First.js
@@ -61,3 +61,18 @@ for(let food of arr){
 }
 
 unorderElement.append(fragment);
+
+const s1 = document.getElementById("first");
+s1.remove();
+
+
+const month = document.getElementById("ten");
+
+// console.log(month.children);
+const lister = document.createElement("li");
+lister.textContent = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>";
+
+// lister.innerHTML = "<img src='https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fHww&fm=jpg&q=60&w=3000'>"
+
+month.prepend(lister);
+// month.insertAdjacentElement("afterend",lister)
