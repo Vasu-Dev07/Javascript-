ef3077a285237f9c5b983cb9e4293ef00a78cd4e
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-02 22:12:03 +0530

    correcct way  for event occurence

--- Starting/Second.js
+++ Starting/Second.js
@@ -13,3 +13,16 @@ const element = document.getElementById("first");
 // element.onclick = function handleClick(){
 //    element.textContent = "I am the best";
 // }
+
+// element.addEventListener('click',()=>{
+//     element.textContent = "Strike is Coming";
+// })
+
+// element.addEventListener('click',()=>{
+//     element.style.backgroundColor="brown";
+// })
+
+// const child1 = document.getElementById("child1");
+// child1.addEventListener('click',()=>{
+//     child1.textContent = "I am clicked";
+// })
