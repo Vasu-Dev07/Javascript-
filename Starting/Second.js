b25d29f28416d824b462347572aeef5b5b0cb027
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-02 22:04:49 +0530

    Events in web using js

---
+++ Starting/Second.js
@@ -0,0 +1,15 @@
+// function handleClick(){
+//     const element = document.getElementById("first");
+//     element.textContent = "Strike is Coming";
+// }
+
+
+const element = document.getElementById("first");
+// element.onclick = function handleClick(){
+// //    element.textContent = "Strike is Coming";
+//     element.style.backgroundColor="pink";
+// }
+
+// element.onclick = function handleClick(){
+//    element.textContent = "I am the best";
+// }
