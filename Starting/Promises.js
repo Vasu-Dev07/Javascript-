9fe9dc6066b2d89717af14580835ec7acdee2db3
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-03 20:34:11 +0530

    Promises

---
+++ Starting/Promises.js
@@ -0,0 +1,28 @@
+// console.log("Hello World Start");
+
+
+// code
+
+// const p1 = fetch("https://api.github.com/users");
+
+
+// // fullfilled , reject
+
+// const p2 = p1.then((response)=>{
+//    return response.json();
+// })
+
+
+// p2.then((data)=>{
+//     console.log(data);
+// })
+
+
+// fetch("https://api.github.com/users")
+// .then((response)=>{
+    
+//    return response.json();
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
