1556513383416b20b4898cde20a810cd6205d1b3
VasuDev987 <bishwjitgorai1@gmail.com>
2026-09-08 23:12:54 +0530

    aysnc is a  function thst always return a promise

---
+++ Starting/Fourth.js
@@ -0,0 +1,16 @@
+// async await
+// aysnc function always return a promise
+// async function greet(){
+//     return "Rohit";
+
+//     // return new Promise((resolve,reject)=>{
+//     //     reject("Rohit");
+//     // })
+// }
+
+// const response = greet();
+// // console.log(response);
+// response.then((data)=>console.log(data))
+// .catch((error)=>{
+//     console.log("Error:", error);
+// // })
